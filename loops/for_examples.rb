db98fa13883ad85ca3ace01e0eb7998140a6ee7a
sgp1986 <sgp1229@gmail.com>
2021-09-09 23:02:45 -0600

    adding loops directory

---
+++ loops/for_examples.rb
@@ -0,0 +1,14 @@
+x = gets.chomp.to_i
+
+for i in 1..x do
+  puts x - i
+end
+puts "done!"
+
+y = [1, 2, 3, 4, 5]
+
+for i in y.reverse do
+  puts i
+end
+
+puts "done!"
---
+++ loops/loop_example.rb
@@ -0,0 +1,3 @@
+loop do 
+  puts "this will keep printing until you hit ctrl + c"
+end
